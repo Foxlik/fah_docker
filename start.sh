cd894292ab70d43526254533659d5c32d5fcb768
Tomas Zvala <fox@murder.cz>
2020-03-12 12:55:43 +0100

    Handle options with dash in their name

--- start.sh
+++ start.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#env ## debug
 echo "<config>" > /fah/config.xml
 for var in "${!FAH_@}"
 do
 	key=${var#FAH_}
+	key=${key/_/-}
 	val=${!var}
 	echo "<${key} value=\"${val}\"/>" >> /fah/config.xml
 done
 echo "</config>" >> /fah/config.xml
-
+#cat /fah/config.xml ## DEBUG
 exec FAHClient
